a41c6a9a5c398e14f1bbfa2df023950ba42c3a26
Joseph Michaels <jmichaels@gmail.com>
2016-07-26 11:25:50 -0700

    Interest rate changes, borrow more and payment added

--- client/components/loan/loan.component.ts
+++ client/components/loan/loan.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, AfterViewInit } from "@angular/core";
+import { Component, Input, ViewChild, AfterViewInit, Pipe, PipeTransform, ChangeDetectorRef } from "@angular/core";
 import { SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES } from "ng-semantic";
 import { Http, Headers, RequestOptions } from "@angular/http";
 import { Loan } from "./loan";
@@ -15,35 +15,84 @@ declare var jQuery;
 })
 export class LoanComponent implements AfterViewInit {
     loans: Loan[] = [];
-	loanIpsum: Loan;
     currentLoan: Loan = null;
 
-    nameField: string = "Loan";
-    idField: number = 1;
-    principleField: number = 10000;
-    rateField: number = 6;
-    datePicker: Date = new Date();
-	period: string = "";
+    nameField 		= "Loan";
+    idField 		= 1;
+    principleField 	= 10000;
+    //rateField 		= 6;
+	_rate			= 6;
+	payField 		= 1000;
+	borrowField		= 10000;
+    datePicker		= DateUtils.getCurrentDate();
+	period			= "";
+
+	showCreatePanel: boolean = true;
+	showControlPanel: boolean = false;
 
 	@ViewChild('startingDate') startingDate;
 
-    constructor(public http: Http) {
+	/*
+	 * Getters and setters that hook into the currently selected loan.
+	 */
+	get rateField(): number {
+		//return this._rate;
+		if (this.currentLoan) {
+			return this._rate;
+		} else {
+			return 0;
+		}
+	}
+
+	set rateField(value: number) {
+		// this._rate = value;
+		// this.rate = (this._rate / 100) + 1;
+		if (this.currentLoan) {
+			this._rate = value;
+			this.currentLoan.rate = value;
+		}
+	}
+
+	get periodSelector(): string {
+		if (this.currentLoan) {
+			return this.currentLoan.period;
+		} else {
+			return "daily";
+		}
+	}
+
+	set periodSelector(value: string) {
+		if (this.currentLoan) {
+			this.currentLoan.period = value;
+		}
+	}
+
+	effectiveRate(): number {
+		if (this.currentLoan) {
+			return this.currentLoan.effectiveRate * 100;
+		} else {
+			return 0;
+		}
+	}
+
+    constructor(public http: Http, public cdRef: ChangeDetectorRef) {
 		let date = DateUtils.getCurrentDate();
 		date.setFullYear(date.getFullYear() - 1);
-		this.loanIpsum = new Loan(0, "Loan Ipsum", 100000, 6, new Date(date.getTime()));
+		this.currentLoan = new Loan(0, "Ipsum Loan", 100000, 6, new Date(date.getTime()));
 
+		let payment1 = Math.round(Math.random() * 1000);
+		let payment2 = Math.round(Math.random() * 1000);
 
-		for (let i=0; i<10; i++) {
+		for (let i=0; i<4; i++) {
 			date.setMonth(date.getMonth() + 1);
 			let localdate = new Date(date.getTime());
-			this.loanIpsum.pay(Math.round(Math.random() * 1000), localdate);
+			this.currentLoan.pay(payment1, localdate);
 		}
-
-		this.currentLoan = this.loanIpsum;
-
-		this.datePicker = DateUtils.getCurrentDate();
-
-		//jQuery('#select').dropdown();
+		for (let i=0; i<5; i++) {
+			date.setMonth(date.getMonth() + 1);
+			let localdate = new Date(date.getTime());
+			this.currentLoan.pay(payment2, localdate);
+		}
 	}
 
 	ngAfterViewInit() {
@@ -51,12 +100,38 @@ export class LoanComponent implements AfterViewInit {
 		jQuery('#datetimepicker1').datetimepicker();
 		this.datePicker = new Date();
 		//jQuery('#periodSelect').dropdown();
+		this.rateFieldChange();
+		this.cdRef.detectChanges();
+	}
+
+	toggleCreateControl() {
+		this.showCreatePanel = false;
+		this.showControlPanel = true;
+	}
+
+	payLoan() {
+		this.currentLoan.pay(this.payField, DateUtils.getCurrentDate());
+	}
+
+	borrowMore() {
+		this.currentLoan.borrow(this.borrowField, DateUtils.getCurrentDate());
+	}
+
+	rateFieldChange() {
+		//console.log("Foobar");
+		// this.rate = (this.rateField / 100) + 1;
+		// this.ratePerPeriod = this.rate / 365;
+		// console.log(this.ratePerPeriod);
+		//this.currentLoan.changeRate(this.rate);
+
+		//this.cdRef.detectChanges();
 	}
 
     createLoan() {
         let loan = new Loan(this.idField, this.nameField, this.principleField, this.rateField, DateUtils.getCurrentDate());
         this.loans.push(loan);
         this.currentLoan = loan;
+		this.toggleCreateControl();
 		//this._appRef.tick();
     }
 
--- client/components/loan/loan.ts
+++ client/components/loan/loan.ts
@@ -1,25 +1,66 @@
 import { DateUtils } from '../../dateutils';
 
 export class Loan {
-    records: LoanRecord[] = [];
+    records: LoanRecord[]   = [];
 
-    interest: number;
-    ratePerPeriod: number;
-    startingPrinciple: number;
+    interest                = 0;
+    ratePerPeriod           = 0;
+    fractionPerPeriod       = 0;
+    startingPrinciple       = 0;
+    _rate                   = 0;
+    effectiveRate           = 0;
 
-    period: string;
+    _period                  = "daily";
 
     constructor(
         public id: number,
         public name: string,
         public principle: number,
-        public rate: number,
+        rate: number,
         public currentDate: Date
     ) {
         this.startingPrinciple = principle;
         this.interest = 0;
-        this.ratePerPeriod = this.rate/365;
+        this.rate = rate;
         this.records.push(new LoanRecord(1, -principle, 0, 0, currentDate, rate, this.getBalance()));
+    }
+
+    set rate(value: number) {
+        this._rate = (value / 100);
+        this.calculateRates();
+    }
+
+    get rate(): number {
+        return this._rate;
+    }
+
+    set period(value: string) {
+        this._period = value;
+        this.calculateRates();
+    }
+
+    get period(): string {
+        return this._period;
+    }
+
+    calculateRates() {
+        this.fractionPerPeriod = this._rate / this.periodsPerYear();
+        this.ratePerPeriod = this.fractionPerPeriod + 1;
+        //console.log("_rate = "+this._rate + ", ratePerPeriod = "+this.ratePerPeriod);
+        this.effectiveRate = Math.pow(this.ratePerPeriod, this.periodsPerYear()) - 1;
+        //console.log("Setting effective rate for loan " + this.name + " to "+this.effectiveRate);
+    }
+
+    periodsPerYear(): number {
+        switch (this.period) {
+            case "daily":
+                return 365;
+            case "monthly":
+                return 12;
+            case "annually":
+                return 1;
+        }
+        return 1;
     }
 
     getBalance(): number {
@@ -27,7 +68,7 @@ export class Loan {
     }
 
     pay(payment: number, date: Date): LoanRecord {
-        let interest = 0;
+        //let interest = 0;
         let id = this.records.length + 1;
 
         let accruedInterest = this.accrue(date);
@@ -54,17 +95,29 @@ export class Loan {
     }
 
     accrue(to: Date): number {
+        console.log("Debug: Starting Accrue");
         let periodInDays = DateUtils.differenceInDays(this.currentDate, to);
-        let coefficient = Math.pow(this.ratePerPeriod, periodInDays);
+        console.log("Debug: "+periodInDays);
+        //let coefficient = Math.pow(this.ratePerPeriod, periodInDays);
+        //console.log("Debug:"+this.effectiveRate;
 
-        let interest = this.getBalance() * coefficient;
+        let interest = this.getBalance() * this.fractionPerPeriod;
         this.interest += interest;
+
+        this.currentDate = to;
 
         return interest;
     }
 
     borrow(amount: number, date: Date) {
+        let accruedInterest = this.accrue(date);
+        let id = this.records.length + 1;
 
+        this.principle += amount;
+
+        let record = new LoanRecord(id, -amount, accruedInterest, 0, date, this.rate, this.getBalance());
+
+        this.records.push(record);
     }
 }
 
